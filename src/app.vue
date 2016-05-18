c277314f2ad55283d501d6df2cf3fcb0af60361b
maadborn <mats.adborn@gmail.com>
2016-05-18 21:35:37 +0200

    initial

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import Hello from './components/hello/hello.vue'
+
+export default {
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
